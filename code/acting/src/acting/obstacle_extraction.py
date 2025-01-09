02078e969bd30ffa73c8deda4b83df26f2204d5e
max grimm <max.grimm2@gmail.com>
2025-01-09 16:37:02 +0100

    get lane information and sort data from furthest to nearest and left /right

--- code/acting/src/acting/obstacle_extraction.py
+++ code/acting/src/acting/obstacle_extraction.py
@@ -2,8 +2,10 @@
 import ros_compatibility as roscomp
 from ros_compatibility.node import CompatibleNode
 from mapping.msg import Map as MapMsg
+from visualization_msgs.msg import Marker, MarkerArray
 from mapping.msg import Entity
 from std_msgs.msg import String
+from geometry_msgs.msg import Pose, Point
 from rospy import Publisher
 
 
@@ -23,18 +25,47 @@ class ObstacleDetection(CompatibleNode):
         self.role_name = self.get_param("role_name", "ego_vehicle")
 
         self.new_subscription(
-            topic=self.get_param("~map_topic", "/paf/hero/mapping/init_data"),
-            msg_type=MapMsg,
+            topic=self.get_param(
+                "~map_topic", "/paf/hero/visualization_marker_array_lane"
+            ),
+            msg_type=MarkerArray,
             callback=self.map_callback,
             qos_profile=1,
         )
 
         self.entity_publisher: Publisher = self.new_publisher(
-            Entity, "/test_entity", qos_profile=1
+            Point, "/test", qos_profile=1
         )
 
     def map_callback(self, data):
-        self.entity_publisher.publish(data.entities[0])
+        # self.entity_publisher.publish(data.markers[0].pose.position)
+
+        lane_points_left = []
+        lane_points_right = []
+
+        for marker in data.markers:
+            # left side of the car is positiv y
+            if (marker.pose.position.y) > 0:
+                lane_points_left.append(marker.pose.position)
+            # right side of the car is negativ y
+            else:
+                lane_points_right.append(marker.pose.position)
+
+            lane_points_left.sort(key=lambda point: point.x)
+            lane_points_right.sort(key=lambda point: point.x)
+            lane_points_left.reverse()
+            lane_points_right.reverse()
+
+        temp = Point()
+        temp.x = -1.0
+        temp.y = -1.0
+        temp.z = -1.0
+        self.entity_publisher.publish(temp)
+
+        self.entity_publisher.publish(lane_points_left[0])
+        self.entity_publisher.publish(lane_points_left[-1])
+        self.entity_publisher.publish(lane_points_right[0])
+        self.entity_publisher.publish(lane_points_right[-1])
 
 
 def main(args=None):
