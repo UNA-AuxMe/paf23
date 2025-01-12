5a4d004a42a33a9512d741a9163448e054b83f9f
vinzenzm <vinzenz@malke.info>
2025-01-12 15:03:57 +0100

    potential planner now uses the intermediate layer

--- code/testbed/potential_field/src/potential_field/p_field_planner.py
+++ code/testbed/potential_field/src/potential_field/p_field_planner.py
@@ -7,18 +7,18 @@ import numpy as np
 from sim.msg import (
     VehicleCtrl,
     VehicleInfo,
-    Entity,
-    Entities,
     Vector2D,
     VisPath,
     MultiPath,
 )
-
+from mapping.msg import Map as MapMsg
+from mapping_common.entity import Entity
+from mapping_common.map import Map
 
 from acting.trajectory_modifier import TrajectoryModifier
 
 
-from typing import Tuple, List
+from typing import Tuple, List, Optional
 from numpy.typing import NDArray
 
 
@@ -41,34 +41,27 @@ class PotentialField(CompatibleNode, TrajectoryModifier):
             callback=self._info_callback,
             qos_profile=10,
         )
+
         self.new_subscription(
-            msg_type=Entities,
-            topic="entities",
-            callback=self._entities_callback,
-            qos_profile=10,
+            topic=self.get_param("~map_topic", "/paf/hero/mapping/init_data"),
+            msg_type=MapMsg,
+            callback=self._map_callback,
+            qos_profile=1,
         )
 
         self.car_position: Tuple[float, float] = (0, 0)
         self.car_theta: float = 0.0
 
-        self.obstacles: List[Entities] = []
+        self.map: Optional[Map] = None
 
         rate_hz = 20
         self.dt = 1.0 / rate_hz
-        self.new_timer(self.dt, lambda _: self.update())
+        self.new_timer(self.dt, lambda _: self._update())
 
-    def force_vector(self, x: float, y: float) -> Tuple[float, float]:
-        cumulative = np.zeros((2))
-        for obstacle in self.obstacles:
-            distance = self._distance(x, y, obstacle)
-            direction = self._direction_towards(x, y, obstacle)
-            if distance == 0:
-                continue
-            cumulative += 2 * direction * 1 / distance
-        return cumulative
+    def _map_callback(self, map_msg: MapMsg):
+        self.map = Map.from_ros_msg(map_msg)
 
-    def update(self):
-        car_x, car_y = self.car_position
+    def _update(self):
         force_x, force_y = self.force_vector(0, 0)  # car is at 0,0 for this
         w_f_x, w_f_y = self._car_to_world(force_x, force_y)
         path: List[Tuple[float, float]] = [
@@ -79,11 +72,23 @@ class PotentialField(CompatibleNode, TrajectoryModifier):
         self.show_multi_path(self.multi_path)
         self.multi_path = []
 
-    def control(self, steer: float, throttle: float, brake: float) -> None:
-        msg = VehicleCtrl()
-        msg.steering = steer
-        msg.throttle = throttle
-        msg.brake = brake
+    def _info_callback(self, msg: VehicleInfo):
+        self.car_position = (msg.x, msg.y)
+        self.car_theta = msg.theta
+
+    def force_vector(self, x: float, y: float) -> Tuple[float, float]:
+        cumulative = np.zeros((2))
+        if self.map is None:
+            return cumulative
+
+        obstacles = self.map.entities_without_hero()
+        for obstacle in obstacles:
+            distance = self._distance(x, y, obstacle)
+            direction = self._direction_towards(x, y, obstacle)
+            if distance == 0:
+                continue
+            cumulative += 2 * direction * 1 / distance
+        return cumulative
 
     def show_multi_path(self, multi_path: List[List[Tuple[float, float]]]) -> None:
         msg = MultiPath()
@@ -95,27 +100,24 @@ class PotentialField(CompatibleNode, TrajectoryModifier):
             msg.paths.append(vis_path)
         self.multi_path_pub.publish(msg)
 
-    def _info_callback(self, msg: VehicleInfo):
-        self.car_position = (msg.x, msg.y)
-        self.car_theta = msg.theta
-
-    def _entities_callback(self, msg: Entities):
-        self.obstacles = msg.entities
-
     def _distance(self, x: float, y: float, ent: Entity):
         pos1 = np.array([x, y])
-        pos2 = self._car_to_world(ent.x, ent.y)
+        x = ent.transform.translation().x()
+        y = ent.transform.translation().y()
+        pos2 = self._car_to_world(x, y)
         return np.linalg.norm(pos1 - pos2)
 
     def _direction_towards(self, x: float, y: float, ent: Entity):
         pos1 = np.array([x, y])
-        pos2 = self._car_to_world(ent.x, ent.y)
+        x = ent.transform.translation().x()
+        y = ent.transform.translation().y()
+        pos2 = self._car_to_world(x, y)
         d: float = np.linalg.norm(pos2 - pos1)
         if d == 0.0:
             return pos2 - pos1
         return (pos2 - pos1) / d
 
-    def _car_to_world(self, x: float, y: float):
+    def _car_to_world(self, x: float, y: float) -> NDArray:
         car_x, car_y = self.car_position
         car_phi = self.car_theta
         translation = np.array([car_x, car_y])
@@ -128,8 +130,8 @@ class PotentialField(CompatibleNode, TrajectoryModifier):
         )  # Rotate and then translate
         return world_position
 
+    # overrides superclass
     def modify_path(self, positions: NDArray) -> bool:
-
         for i, position in enumerate(positions):
             x, y = position
             force = self.force_vector(x, y)
